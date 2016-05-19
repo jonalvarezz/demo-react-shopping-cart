2eb1135e04f4205137ec918ef2e30f3be8f7c500
Jonathan Alvarez G <j@jonalvarezz.com>
2016-05-18 23:41:01 -0500

    08 - Make it composable

--- app/index.js
+++ app/index.js
@@ -3,6 +3,26 @@ var React = require('react')
 var render = require('react-dom').render
 var API = require('./api')
 
+var Product = React.createClass({
+  propTypes: {
+    id: React.PropTypes.any.isRequired,
+    image: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string.isRequired,
+    price: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+    var props = this.props
+    return (
+      <li>
+        <img src={props.image} alt={props.title} />
+        <h2>{props.title}</h2>
+        <p>$ {props.price}</p>
+      </li>
+    )
+  }
+})
+
 var ProductList = React.createClass({
   propTypes: {
     products: React.PropTypes.array.isRequired
@@ -13,13 +33,7 @@ var ProductList = React.createClass({
     return (
       <ul className="product-list">
         {props.products.map(function (product) {
-          return (
-            <li key={product.id}>
-              <img src={product.image} alt={product.title} />
-              <h2>{product.title}</h2>
-              <p>$ {product.price}</p>
-            </li>
-          )
+          return <Product key={product.id} {...product} />
         })}
       </ul>
     );
