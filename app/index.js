20bd7585f9a713bd0cf21cd61cb9a98b1ad50bab
Jonathan Alvarez G <j@jonalvarezz.com>
2016-05-18 23:16:49 -0500

    04 - Introducing React components and props

--- app/index.js
+++ app/index.js
@@ -2,7 +2,16 @@
 var React = require('react')
 var render = require('react-dom').render
 
+// First React component receiving props
+var Heading1 = React.createClass({
+  render: function () {
+    return (
+      <h1>{this.props.content}</h1>
+    )
+  }
+})
+
 render(
-  <h1>Hello Pereira JS from React</h1>,
+  <Heading1 content={'Hello Pereira JS from React'} />,
   document.getElementById('app')
 )
